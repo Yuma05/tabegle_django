8eb5ca2921775427bad5c31fd4179d4bd2da7684
yuma <yuuma1403@gmail.com>
2020-11-23 15:16:19 +0900

    change for heroku

--- tabegle/settings.py
+++ tabegle/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -135,7 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # メールはコンソールに表示
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
